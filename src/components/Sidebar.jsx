dd1da2d1347f7456a0d7d2e1ae43ea2c93f93ab9
raizafr <frraiza056@gmail.com>
2023-06-03 15:38:40 +0700

    perbaikan sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,194 +1,56 @@
 import SidebarLink from "./SidebarLink";
+import ProfilePicture from "../assets/profile_default.png";
+
+import { AiOutlineHome, AiFillTwitterSquare } from "react-icons/ai";
+import { BiHash } from "react-icons/bi";
+import { RiNotification2Line, RiFileListLine } from "react-icons/ri";
+import { FiMail } from "react-icons/fi";
+import { BsBookmark } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
+import { HiOutlineUser } from "react-icons/hi";
+import { BsTwitter } from "react-icons/bs";
+import { TfiWrite } from "react-icons/tfi";
+import { LuMoreHorizontal } from "react-icons/lu";
 
 const Sidebar = () => {
   return (
     <div className="hidden sm:flex flex-col px-4 md:px-5 lg:px-14 b-slate-400 max-h-screen">
       <div
-        className="w-fit hover:bg-zinc-900 p-3 rounded-full
+        className="w-fit hover:bg-zinc-900 p-4 rounded-full
       "
       >
-        <img
-          src="https://rb.gy/ogau5a"
-          alt=""
-          width={"29px"}
-          height={"29px"}
-          className=""
-        />
+        <BsTwitter className="fill-white scale-[1.7]" />
       </div>
-      <div className="">
+      <div className="space-y-0.5">
         <SidebarLink
           title="Home"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-7 h-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-              />
-            </svg>
-          }
+          icon={<AiOutlineHome className=" scale-125" />}
         />
-        <SidebarLink
-          title="Explore"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-7 h-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5"
-              />
-            </svg>
-          }
-        />
+        <SidebarLink title="Explore" icon={<BiHash className=" scale-125" />} />
         <SidebarLink
           title="Notification"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-7 h-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-              />
-            </svg>
-          }
+          icon={<RiNotification2Line className="scale-125" />}
         />
-        <SidebarLink
-          title="Massage"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-7 h-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-              />
-            </svg>
-          }
-        />
+        <SidebarLink title="Massage" icon={<FiMail className="scale-125" />} />
         <SidebarLink
           title="Lists"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-7 h-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
-              />
-            </svg>
-          }
+          icon={<RiFileListLine className="scale-125" />}
         />
         <SidebarLink
           title="Bookmarks"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-7 h-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-              />
-            </svg>
-          }
+          icon={<BsBookmark className="scale-125" />}
         />
         <SidebarLink
           title="Twitter Blue"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-7 h-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 4.5v15m6-15v15m-10.875 0h15.75c.621 0 1.125-.504 1.125-1.125V5.625c0-.621-.504-1.125-1.125-1.125H4.125C3.504 4.5 3 5.004 3 5.625v12.75c0 .621.504 1.125 1.125 1.125z"
-              />
-            </svg>
-          }
+          icon={<AiFillTwitterSquare className="scale-125" />}
         />
         <SidebarLink
           title="Profile"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-7 h-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-              />
-            </svg>
-          }
+          icon={<HiOutlineUser className="scale-125" />}
         />
-        <SidebarLink
-          title="More"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-7 h-7"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          }
-        />
+        <SidebarLink title="More" icon={<CgMoreO className="scale-125" />} />
       </div>
-      <div className="mt-2">
+      <div className="lg:mt-2 md:mt-6">
         <a
           href=""
           className="text-xl bg-[#1D9BF0] text-white py-2.5 px-20 rounded-full hover:bg-[#146AFA] hidden lg:block w-fit"
@@ -199,31 +61,18 @@ const Sidebar = () => {
           href=""
           className="text-xl bg-[#1D9BF0] text-white p-2 rounded-full hover:bg-[#146AFA] hidden md:block w-fit lg:hidden ml-1"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-            />
-          </svg>
+          <TfiWrite />
         </a>
       </div>
       <div className="relative h-full text-white">
         <div className="absolute inset-x-0 bottom-4 flex justify-between hover:bg-zinc-900 px-2 rounded-full py-1">
           <div className="flex">
             <div
-              className="w-fit hover:bg-zinc-900 p-3 rounded-full
+              className="w-fit hover:bg-zinc-900 lg:p-3 rounded-full md:p-1
       "
             >
               <img
-                src="https://rb.gy/ogau5a"
+                src={ProfilePicture}
                 alt=""
                 width={"29px"}
                 height={"29px"}
@@ -235,21 +84,8 @@ const Sidebar = () => {
               <p className="text-sm opacity-50">@FrRaiza33490</p>
             </div>
           </div>
-          <div className="mt-3 hidden lg:block">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-              />
-            </svg>
+          <div className="mt-4 hidden lg:block">
+            <LuMoreHorizontal className="scale-125" />
           </div>
         </div>
       </div>
--- src/components/SidebarLink.jsx
+++ src/components/SidebarLink.jsx
@@ -1,7 +1,7 @@
 const SidebarLink = (props) => {
   return (
-    <div className="text-white flex space-x-4 text-xl hover:bg-zinc-900 w-fit px-2.5 py-2.5 rounded-full">
-      <div className="mt-0.5">{props.icon}</div>
+    <div className="text-white flex lg:space-x-4 text-xl hover:bg-zinc-900 w-fit px-3 py-2.5 rounded-full">
+      <div className="mt-1">{props.icon}</div>
       <div className="sm:hidden lg:block">{props.title}</div>
     </div>
   );
